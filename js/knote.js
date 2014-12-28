4417958910e35bd10385588c1daf07d1b56791d1
Gupocca <gupocca@gmail.com>
2014-12-27 16:11:01 -0800

    Linted the Javascript

--- js/knote.js
+++ js/knote.js
@@ -3,19 +3,47 @@ var reader = new stmd.DocParser();
 var defaultNotepad = 'default';
 var currentNotepad = defaultNotepad;
 
-// bulk of the code
-$(document).ready(function() {
+var tagBody, tagOrComment, removeTags;
 
-	var populateNotepad = function() { 
+// bulk of the code
+$(document).ready(function () {
+	'use strict';
+	var client, allData, getNotepadNames, populateNotepad, renderNotepadSelection, isEmptyOrSpaces, createNewNotepad, checkAuth, showError, hideError, openDataChannels, setData, getData, deleteNotepad, isExistingNotepad, strip, renderMath, renderCenteredMath, charExists, renderTex;
+
+	client = new Dropbox.Client({key: "7nj69doyzp49ge1"});
+	allData = null;
+
+	getNotepadNames = function () {
+		var i, names, results, notepadTable;
+		if (allData === null) {
+			return '';
+		}
+
+		notepadTable = allData.getTable('notepads');
+		results = notepadTable.query({exists: true});
+
+		if (results.length > 0) {
+			names = [];
+
+			for (i = 0; i < results.length; i++) {
+				names[i] = results[i].get('padname');
+			}
+
+			return names;
+		}
+	};
+
+	populateNotepad = function () {
+		var names, incoming;
+
 		// check if we're using the default notepad
-		if (currentNotepad == defaultNotepad) {
-			var names = getNotepadNames();
+		if (currentNotepad === defaultNotepad) {
+			names = getNotepadNames();
 			if (names.length > 0) {
 				currentNotepad = names[0];
-			}
-			else {
+			} else {
 				// there are no notepads; create the default one
-				$.get("defaultFile.md", function(input) {
+				$.get('defaultFile.md', function (input) {
 					setData(defaultNotepad, input);
 				});
 			}
@@ -23,23 +51,22 @@ $(document).ready(function() {
 
 		$('#current-notepad').text(currentNotepad);
 
-		var incoming = getData(currentNotepad);
-		if (incoming != $('#tex').val()) {
+		incoming = getData(currentNotepad);
+		if (incoming !== $('#tex').val()) {
 			$('#tex').val(getData(currentNotepad));
 			renderTex();
 		}
 
 		renderNotepadSelection();
-	}
+	};
 
-	var renderNotepadSelection = function() {
-		var names = getNotepadNames();
-		var output = '<ul>';
+	renderNotepadSelection = function () {
+		var names = getNotepadNames(), output = '<ul>', name, i;
 
-		for (var i = 0; i < names.length; i++) {
-			var name = removeTags(names[i]);
-			output += '<li><a class="swap-notepad" href="#" data-dismiss="modal">'+name+'</a>'
-					+ '<a class="remove-notepad" href="#" data-notepad="'+name+'"><i class="fa fa-times"></i></a></li>';
+		for (i = 0; i < names.length; i++) {
+			name = removeTags(names[i]);
+			output += '<li><a class="swap-notepad" href="#" data-dismiss="modal">' + name + '</a>'
+					+ '<a class="remove-notepad" href="#" data-notepad="' + name + '"><i class="fa fa-times"></i></a></li>';
 		}
 		output += '</ul>';
 		$('#notepad-selection').html(output);
@@ -52,11 +79,11 @@ $(document).ready(function() {
 		});
 
 		$('#notepad-selection li a.remove-notepad').click(function () {
-			var name = $(this).data('notepad');
-			var confirmation = confirm('Are you sure you want to delete notepad "'+name+'"?');
+			name = $(this).data('notepad');
+			var confirmation = confirm('Are you sure you want to delete notepad "' + name + '"?');
 
 			if (confirmation) {
-				if (name == currentNotepad) {
+				if (name === currentNotepad) {
 					currentNotepad = defaultNotepad;
 				}
 				deleteNotepad(name);
@@ -65,13 +92,13 @@ $(document).ready(function() {
 			populateNotepad();
 			return false;
 		});
-	}
+	};
 
-	function isEmptyOrSpaces(str){
+	isEmptyOrSpaces = function (str) {
 		return str === null || str.match(/^ *$/) !== null;
-	}
+	};
 
-	var createNewNotepad = function() {
+	createNewNotepad = function () {
 		var name = $('#new-notepad-name').val();
 		$('#new-notepad-name').val('');
 
@@ -81,65 +108,59 @@ $(document).ready(function() {
 
 		if (isExistingNotepad(name)) {
 			alert("notepad already exists");
-		}
-		else {
-			setData(name,'');
+		} else {
+			setData(name, '');
 			renderNotepadSelection();
-		}	
-	}
+		}
+	};
 
 	$('#notepad-creation').submit(function () {
 		createNewNotepad();
 		return false;
 	});
 
-	var showError = function(err) {
+	showError = function (err) {
 		$('#errors').css('display', 'block');
 		$('#alert').html("<strong>Error!</strong> " + err);
-	}
+	};
 
-	var hideError = function() {
+	hideError = function () {
 		$('#errors').css('display', 'none');
-	}
-
-	var client = new Dropbox.Client({key: "7nj69doyzp49ge1"});
-	var allData = null;
+	};
 
 	client.authenticate({interactive: false}, function (error) {
 		if (error) {
-			showError("Unable to authenticate.")
+			showError("Unable to authenticate.");
 			$('#dropbox-button').addClass('disabled');
 		}
 	});
 
-	var checkAuth = function() {
+	checkAuth = function () {
 		if (client.isAuthenticated()) {
-			$('#dropbox').css('display','none');
-			$('#app').css('display','block');
-			$('#current-notepad').css('display','inline-block');
+			$('#dropbox').css('display', 'none');
+			$('#app').css('display', 'block');
+			$('#current-notepad').css('display', 'inline-block');
 		}
 	};
 
-	checkAuth();
-
-	$('#dropbox-button').click(function() {
+	$('#dropbox-button').click(function () {
 		client.authenticate({}, function (error) {
 			if (error) {
-				showError("Unable to authenticate.")
+				showError('Unable to authenticate.');
 				$('#dropbox-button').addClass('disabled');
 			}
 			checkAuth();
 		});
 	});
 
-	var openDataChannels = function() {
+	openDataChannels = function () {
 		console.log("Channels");
 		var datastoreManager = client.getDatastoreManager();
 		datastoreManager.openDefaultDatastore(function (error, datastore) {
 			if (error) {
 				showError("Unable to access data.");
 			}
-			
+
 			allData = datastore;
 
 			datastore.recordsChanged.addListener(function (event) {
@@ -148,190 +169,174 @@ $(document).ready(function() {
 
 			populateNotepad();
 		});
-	}
+	};
 
-	openDataChannels();
+	setData = function (key, newData) {
+		var notepadTable, results, notepad;
 
-	var setData = function(key, newData) {
 		if (allData === null) {
 			return;
 		}
 
-		var notepadTable = allData.getTable('notepads');
-		var results = notepadTable.query({padname: key});
+		notepadTable = allData.getTable('notepads');
+		results = notepadTable.query({padname: key});
 
 		if (results.length > 0) {
-			var notepad = results[0];
+			notepad = results[0];
 			notepad.set('data', newData);
-		}
-		else {
-			var newNotepad = notepadTable.insert({
+		} else {
+			notepadTable.insert({
 				padname: key,
 				data: newData,
 				exists: true,
 				created: new Date()
 			});
 		}
-	}
+	};
 
-	var getData = function(key) {
+	getData = function (key) {
+		var notepadTable, results;
+
 		if (allData === null) {
 			return '';
 		}
 
-		var notepadTable = allData.getTable('notepads');
-		var results = notepadTable.query({padname: key});
+		notepadTable = allData.getTable('notepads');
+		results = notepadTable.query({padname: key});
 
 		if (results.length > 0) {
 			return results[0].get('data');
 		}
 		return '';
-	}
+	};
 
-	var deleteNotepad = function(key) {
+	deleteNotepad = function (key) {
+		var notepadTable, results;
+
 		if (allData === null) {
 			return;
 		}
 
-		var notepadTable = allData.getTable('notepads');
-		var results = notepadTable.query({padname: key});
+		notepadTable = allData.getTable('notepads');
+		results = notepadTable.query({padname: key});
 
 		if (results.length > 0) {
 			results[0].deleteRecord();
 		}
-	}
+	};
 
-	var deleteAllNotepads = function(key) {
-		var names = getNotepadNames();
-		for (var i = 0; i < names.length; i++)
-		{
-			deleteNotepad(names[i]);
-		}
-	}
+	// var deleteAllNotepads = function (key) {
+	//     var i, names = getNotepadNames();
+	//     for (i = 0; i < names.length; i++) {
+	//         deleteNotepad(names[i]);
+	//     }
+	// };
 
-	var isExistingNotepad = function(key) {
+	isExistingNotepad = function (key) {
+		var notepadTable, results;
+
 		if (allData === null) {
 			return false;
 		}
 
-		var notepadTable = allData.getTable('notepads');
-		var results = notepadTable.query({padname: key});
+		notepadTable = allData.getTable('notepads');
+		results = notepadTable.query({padname: key});
 		return results.length > 0;
-	}
-
-	var getNotepadNames = function() {
-		if (allData === null) {
-			return '';
-		}
-
-		var notepadTable = allData.getTable('notepads');
-		var results = notepadTable.query({exists: true});
-
-		if (results.length > 0) {
-			var names = new Array();
-
-			for (var i = 0; i < results.length; i++)
-			{
-				names[i] = results[i].get('padname');
-			}
-
-			return names;
-		}
-		else {
-			return '';
-		}
-	}
+	};
 
 	// utility function
-	function strip(html)
-	{
+	strip = function (html) {
 		var tmp = document.createElement("div");
 		tmp.innerHTML = html;
 		return tmp.textContent || tmp.innerText || "";
-	}
-
-	String.prototype.endsWith = function(suffix) {
-	    return this.indexOf(suffix, this.length - suffix.length) !== -1;
 	};
 
-	var renderMath = function(data) {
+	String.prototype.endsWith = function (suffix) {
+		return this.indexOf(suffix, this.length - suffix.length) !== -1;
+	};
+
+	renderMath = function (data) {
 		return katex.renderToString(strip(data));
-	}
-
-	var renderCenteredMath = function(data) {
-		return '<div class="text-center">' + katex.renderToString('\\displaystyle {'+strip(data)+'}') + '</div>';
-	}
-
-	var charExists = function(str, index, value) {
-		return index >= 0 && index < str.length && str.charAt(index) == value;
 	};
 
-	function replaceAll(string, find, replace) {
-		return string.replace(new RegExp(escapeRegExp(find), 'g'), replace);
-	}
+	renderCenteredMath = function (data) {
+		return '<div class="text-center">' + katex.renderToString('\\displaystyle {' + strip(data) + '}') + '</div>';
+	};
 
-	var renderTex = function() {
+	charExists = function (str, index, value) {
+		return index >= 0 && index < str.length && str.charAt(index) === value;
+	};
+
+	renderTex = function () {
+		var input, index, prevIndex, normalMathMode, centeredMathMode, output, centeredToken, segment;
 		hideError();
 
-		var math = document.getElementById("output");
-		var input = $('#tex').val();
+		input = $('#tex').val();
 
 		setData(currentNotepad, input);
-		input = input.split('\\\$').join('\\\\\$')	
+		input = input.split('\\\$').join('\\\\\$');
 		input = writer.renderBlock(reader.parse(input));
 
-		var index = -1;
-		var prevIndex = -1;
-		var normalMathMode = false;
-		var centeredMathMode = false;
-		var output = '';
+		index = -1;
+		prevIndex = -1;
+		normalMathMode = false;
+		centeredMathMode = false;
+		output = '';
 
 		try {
-			while ((index = input.indexOf('$', index+1)) != -1) {
+			while ((index = input.indexOf('$', index + 1)) !== -1) {
 				/* ESCAPES */
-				if (charExists(input, index-1, '\\')) {
+				if (charExists(input, index - 1, '\\')) {
 					continue;
 				}
 
 				/* DOUBLE SIGNS */
-				var centeredToken = false;
-				if (charExists(input, index+1, '$')) {
-					centeredToken = centeredMathMode || !charExists(input, index+2, '$') || index+1 == input.length-1;
+				centeredToken = false;
+				if (charExists(input, index + 1, '$')) {
+					centeredToken = centeredMathMode || !charExists(input, index + 2, '$') || index + 1 === input.length - 1;
 				}
 
-				var segment = input.slice(prevIndex + 1, index);
+				segment = input.slice(prevIndex + 1, index);
 
 				/* CASES */
 				if (normalMathMode) { // starting in math mode
 					output += renderMath(segment);
 					normalMathMode = false;
-				}
-				else if (centeredMathMode) { // starting in centered mode
+				} else if (centeredMathMode) { // starting in centered mode
 					output += renderCenteredMath(segment);
 					centeredMathMode = false;
-				}
-				else { // starting in normal mode
+				} else { // starting in normal mode
 					output += segment;
-					if (centeredToken) { centeredMathMode = true; }
-					else { normalMathMode = true; }
+					if (centeredToken) {
+						centeredMathMode = true;
+					} else {
+						normalMathMode = true;
+					}
 				}
 
 				if (centeredToken) { index++; }
 				prevIndex = index;
 			}
 
-			var segment = input.slice(prevIndex + 1);
-			if (normalMathMode) { output += renderMath(segment); }
-			else if (centeredMathMode) { output += renderCenteredMath(segment); }
-			else { output += segment; }
+			segment = input.slice(prevIndex + 1);
+
+			if (normalMathMode) {
+				output += renderMath(segment);
+			} else if (centeredMathMode) {
+				output += renderCenteredMath(segment);
+			} else {
+				output += segment;
+			}
 
 			$('#output').html(output.split('\\$').join('$'));
-		}
-		catch (err) {
+		} catch (err) {
 			showError(err.message);
 		}
-	}
-	
+	};
+
+	checkAuth();
+	openDataChannels();
+
 	// autofocus
 	$('#tex').focus();
 
@@ -341,14 +346,15 @@ $(document).ready(function() {
 });
 
 // sanitization
-var tagBody = '(?:[^"\'>]|"[^"]*"|\'[^\']*\')*';
-var tagOrComment = new RegExp('<(?:!--(?:(?:-*[^->])*--+|-?)|script\\b'+tagBody+'>[\\s\\S]*?</script\\s*|style\\b'+tagBody +'>[\\s\\S]*?</style\\s*|/?[a-z]'+tagBody+')>','gi');
+tagBody = '(?:[^"\'>]|"[^"]*"|\'[^\']*\')*';
+tagOrComment = new RegExp('<(?:!--(?:(?:-*[^->])*--+|-?)|script\\b' + tagBody + '>[\\s\\S]*?</script\\s*|style\\b' + tagBody  + '>[\\s\\S]*?</style\\s*|/?[a-z]' + tagBody + ')>', 'gi');
 
-function removeTags(html) {
+removeTags = function (html) {
+	'use strict';
 	var oldHtml;
 	do {
 		oldHtml = html;
 		html = html.replace(tagOrComment, '');
 	} while (html !== oldHtml);
 	return html.replace(/</g, '&lt;');
-}
+};
