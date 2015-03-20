f969827ffc89129a363ea50ab513c53ca82bfed3
Gupocca <gupocca@gmail.com>
2015-03-20 12:13:38 -0700

    Notebook selector is now rendering properly

--- js/utility.js
+++ js/utility.js
@@ -1,8 +1,20 @@
-function showError(err) {
-    $('#errors').css('display', 'block');
-    $('#alert').html('<strong>Error!</strong> ' + err);
+var util = {
+    tagBody: '(?:[^"\'>]|"[^"]*"|\'[^\']*\')*',
+    tagOrComment: new RegExp('<(?:!--(?:(?:-*[^->])*--+|-?)|script\\b' + this.tagBody + '>[\\s\\S]*?</script\\s*|style\\b' + this.tagBody  + '>[\\s\\S]*?</style\\s*|/?[a-z]' + this.tagBody + ')>', 'gi'),
+    removeTags: function(html) {
+        'use strict';
+        var oldHtml;
+        do {
+            oldHtml = html;
+            html = html.replace(this.tagOrComment, '');
+        } while (html !== oldHtml);
+        return html.replace(/</g, '&lt;');
+    },
+    showError: function(err) {
+        $('#errors').css('display', 'block');
+        $('#alert').html('<strong>Error!</strong> ' + err);
+    }
 }
-
 function DataClient(apiKey, changeCallback, authCallback) {
     console.log('creating DataClient');
     this.client = new Dropbox.Client({key: apiKey});
@@ -45,7 +57,7 @@ DataClient.prototype.initialize = function() {
 
     datastoreManager.openDefaultDatastore(function (error, datastore) {
         if (error) {
-            showError('Unable to access data.');
+            util.showError('Unable to access data.');
         }
 
         _this.data = datastore;
@@ -128,14 +140,48 @@ $(document).ready(function () {
             // renderTex();
         }
 
-        // renderNotepadSelection();
+        renderNotepadSelector(client);
+    }
+
+    function renderNotepadSelector(client) {
+        var names = client.getPadNames();
+
+        var list = names.map(function(x) {
+            var name = util.removeTags(x);
+            return '<li><a class="swap-notepad" href="#" data-dismiss="modal">' + name + '</a>'
+                    + '<a class="remove-notepad" href="#" data-notepad="' + name + '"><i class="fa fa-times"></i></a></li>';
+        }).join('');
+
+        $('#notepad-selection').html('<ul>' + list + '</ul>');
+
+        $('#notepad-selection li a.swap-notepad').click(function () {
+            currentNotepad = $(this).text();
+            populateNotepad(client);
+            $('#notepadModal').modal('hide');
+            return false;
+        });
+
+        $('#notepad-selection li a.remove-notepad').click(function () {
+            name = $(this).data('notepad');
+            var confirmation = confirm('Are you sure you want to delete notepad "' + name + '"?');
+
+            if (confirmation) {
+                if (name === currentNotepad) {
+                    currentNotepad = defaultNotepad;
+                }
+                deleteNotepad(name);
+            }
+
+            populateNotepad(client);
+            return false;
+        });
     }
 
     var client = new DataClient('7nj69doyzp49ge1', populateNotepad, function(error, isAuthenticated) {
         console.log('isAuthenticated = ' + isAuthenticated);
 
         if (error) {
-            showError('Unable to authenticate.');
+            util.showError('Unable to authenticate.');
             $('#dropbox-button').addClass('disabled');
         } else if (isAuthenticated) {
             $('#dropbox').css('display', 'none');
